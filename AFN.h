a51d7f1018a3de3c1363a0465622b8672b29804e
Roxana Herrera <roseherrera139@rocketmail.com>
2018-11-08 23:50:04 -0600

    Añade definiciones de la estructura AFN y relacionadas.

---
+++ AFN.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <stdbool.h>
+#include "Bits/conjunto.h"
+
+struct AFN_Automata;
+
+struct AFN_Estado
+{
+	int id; 
+	bool es_final;
+	conjunto *transiciones;
+	conjunto transiciones_epsilon;
+	struct AFN_Automata *automata_contenedor;
+};
+
+struct AFN_Alfabeto
+{
+	char caracter_min; 
+	char caracter_max;
+}; 
+
+struct AFN_Automata
+{
+	int id_estado_inicial; 
+	int no_estados; 
+	struct AFN_Alfabeto alfabeto; 
+	struct AFN_Estado *estados; 
+}; 
+
+// P R O T O T I P O S
+
+/* Lee un AFN de un archivo */ 
+struct AFN_Automata* Leer_AFN (char *nombre_archivo);
+
+/* Obtiene el rango de un alfabeto */ 
+int Rango_Alfabeto (struct AFN_Alfabeto); 
+
+void Imprimir_Estado(struct AFN_Estado);
